8fdb5d691c7c255f0c35bb559c0e2ce80ee2cbd4
wockhardsippin <andrey.naasulin@gmail.com>
2026-01-26 13:15:13 +0400

    fix: reduce navbar padding on mobile to prevent text cutoff

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -257,7 +257,7 @@ export default function Navbar() {
                         : 'none'
                 }}
             >
-                <div className="max-w-[1200px] mx-auto px-4 md:px-6">
+                <div className="max-w-[1200px] mx-auto px-3 md:px-6">
                     <div className="flex items-center justify-between h-16">
                         {/* Logo */}
                         <Link href="/" className="flex items-center z-50 relative">
